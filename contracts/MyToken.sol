76736ae0e6a7c2ba9a4bae8f6c00421d11895178
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-02-10 16:01:31 +0100

    second commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
+    constructor() ERC20("DltCoin", "DLTC") {
         _mint(msg.sender, 1000000 * 10**decimals());
     }
 }
+ 
---
+++ contracts/QuickNodeToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.17;
+
+// ERC20 Token Standard #20 interface
+interface ERC20Interface {
+    function totalSupply () external views returns (uint);
+    function balanceOf(address account) external view returns(uint balance);
+    function allowance(address owner, address spender) external view returns (uint remaining);
+    function transfer(address recipient, uint amount) external returns (bool success);
+    function approve(address spender, uint amount) external returns(bool success);
+    function transferFrom(address sender, address recipient, uint amount) external returns(bool success);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+// Actual token contact
+abstract contract QuickNodeToken  is ERC20Interface {
+   
+ }
