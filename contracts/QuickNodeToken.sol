c429ac2bf8aa480b3718100ff68cb1bca904e426
ameer017 <rajiabdullahi907@gmail.com>
2024-02-12 14:02:05 +0100

    interaction between the contracts

--- contracts/QuickNodeToken.sol
+++ contracts/QuickNodeToken.sol
@@ -4,74 +4,102 @@ pragma solidity ^0.8.17;
 
 // ERC20 Token Standard #20 interface
 interface ERC20Interface {
+    function totalSupply() external view returns (uint);
 
-    function totalSupply () external view returns(uint);
-    function balanceOf(address account) external view returns(uint balance);
-    function allowance(address owner, address spender) external view returns(uint remaining);
-    function transfer(address recipient, uint amount) external returns(bool success);
-    function approve(address spender, uint amount) external returns(bool success);
-    function transferFrom(address sender, address recipient, uint amount) external returns(bool success);
+    function balanceOf(address account) external view returns (uint balance);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint remaining);
+
+    function transfer(
+        address recipient,
+        uint amount
+    ) external returns (bool success);
+
+    function approve(
+        address spender,
+        uint amount
+    ) external returns (bool success);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 }
 
 // Actual token contact
-    
 
-  abstract contract QuickNodeToken  is ERC20Interface {
+abstract contract QuickNodeToken is ERC20Interface {
     string public symbol;
     string public name;
     uint8 public decimals;
     uint public _totalSupply;
 
-  mapping(address => uint) balances;
-  mapping(address => mapping(address => uint)) allowed;
-    
-     constructor() {
-      symbol = "DLTC";
-      name = "DLTCoin";
-      decimals = 18;
-      _totalSupply = 1_000_000_000_000_000_000_000_000; 
-      balances[0xC50458623520eE0e704Bc63040EF0bb388221D1F] = _totalSupply;
-      emit Transfer(address(0), 0xC50458623520eE0e704Bc63040EF0bb388221D1F, _totalSupply);
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
 
+    constructor() {
+        symbol = "DLTC";
+        name = "DLTCoin";
+        decimals = 18;
+        _totalSupply = 1_000_000_000_000_000_000_000_000;
+        balances[0xC50458623520eE0e704Bc63040EF0bb388221D1F] = _totalSupply;
+        emit Transfer(
+            address(0),
+            0xC50458623520eE0e704Bc63040EF0bb388221D1F,
+            _totalSupply
+        );
     }
-    
-    
-   function totalSupply()   public view returns(uint){
-    return _totalSupply - balances[address(0)]; 
-  }
 
-  function balanceOf(address account) public view returns(uint balance){
-    return balances[account];
-  }
+    function totalSupply() public view returns (uint) {
+        return _totalSupply - balances[address(0)];
+    }
 
+    function balanceOf(address account) public view returns (uint balance) {
+        return balances[account];
+    }
 
-    function transfer(address recipient, uint amount) public returns(bool success) {
-    balances[msg.sender] = balances[msg.sender] - amount;
-    balances[recipient] = balances[recipient] + amount;
-    emit Transfer(msg.sender, recipient, amount);
-    return true;
-  }
+    function transfer(
+        address recipient,
+        uint amount
+    ) public returns (bool success) {
+        balances[msg.sender] = balances[msg.sender] - amount;
+        balances[recipient] = balances[recipient] + amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
 
-  function  approve(address spender, uint amount) public returns(bool success) {
-    allowed[msg.sender][spender]  = amount;
-    emit Approval(msg.sender, spender, amount);
-    return true;
-  }
-  
-  function transferFrom(address sender, address recipient, uint amount) public returns(bool success) {
-      balances[sender] = balances[sender] - amount;
-      allowed[sender][msg.sender] = allowed[sender][msg.sender] - amount;
-      balances[recipient] = balances[recipient] + amount;
-      emit Transfer( sender, recipient, amount);
-      return true;
-   }
-  
-   function allowance(address owner, address spender) public view returns(uint remaining) {
-    return allowed[owner][spender];
-   }
+    function approve(
+        address spender,
+        uint amount
+    ) public returns (bool success) {
+        allowed[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
 
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) public returns (bool success) {
+        balances[sender] = balances[sender] - amount;
+        allowed[sender][msg.sender] = allowed[sender][msg.sender] - amount;
+        balances[recipient] = balances[recipient] + amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
 
- }
+    function allowance(
+        address owner,
+        address spender
+    ) public view returns (uint remaining) {
+        return allowed[owner][spender];
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,10 +7,24 @@
 import hre from "hardhat";
 
 
-async function main() {
+async function deployQuickNodeToken() {
   const QuickNodeToken = await hre.ethers.deployContract('QuickNodeToken');
   await QuickNodeToken.waitForDeployment();
   console.log(`QuickNodeToken deployed to ${QuickNodeToken.target}`);
+}
+
+async function deployAnotherContract() {
+  const SimpleDEX = await hre.ethers.deployContract('SimpleDEX');
+  await SimpleDEX.waitForDeployment();
+  console.log(`AnotherContract deployed to ${SimpleDEX.target}`);
+}
+
+async function main() {
+  // Deploy QuickNodeToken contract
+  await deployQuickNodeToken();
+
+  // Deploy AnotherContract contract
+  await deployAnotherContract();
 }
 
 main().catch((error) => {
