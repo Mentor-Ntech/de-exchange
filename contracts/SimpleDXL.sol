a192b946bb674a7bf76b83101d09c65f30d980b4
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-02-12 14:22:46 +0100

    update-on simpleDXL

--- contracts/SimpleDXL.sol
+++ contracts/SimpleDXL.sol
@@ -14,9 +14,25 @@ contract SimpleDEX {
         usdcAddress = _usdcAddress;
     }
 
-    function tradeUSDT(uint256 amount) external {
-        IERC20(usdtAddress).transferFrom(msg.sender, address(this), amount);
-        IERC20(tokenAddress).transfer(msg.sender, amount);
+    function tradeUSDT(uint256 usdtAmount) external {
+        IERC20(usdtAddress).transferFrom(msg.sender, address(this), usdtAmount);
+        IERC20(tokenAddress).transfer(msg.sender, usdtAmount);
+        
+        require(usdtAmount > 0, "invalid usdt amount");
+
+        // Ensure that the sender has approved this contract to spend their USDT tokens
+        IERC20 usdtToken = IERC20(usdtAddress);
+        require(
+            usdtToken.allowance(msg.sender, address(this)) >= usdtAmount,
+            "USDT allowance not set"
+        );
+
+        // Transfer the specified amount of USDT tokens from the sender to this contract
+        require(
+            usdtToken.transferFrom(msg.sender, address(this), usdtAmount),
+            "USDT transfer failed"
+        );
+
     }
 
     function tradeUSDC(uint256 amount) external {
@@ -28,4 +44,6 @@ contract SimpleDEX {
         require(msg.sender == tokenAddress, "Only the token contract can withdraw tokens");
         IERC20(token).transfer(msg.sender, amount);
     }
+
+
 }
