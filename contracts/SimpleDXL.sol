5352a6b3503b3a50d9e3d3547276f86bfff7ce74
muhmingreat <ayatullahmumin3@gmail.com>
2024-02-12 14:04:12 +0100

    update

--- contracts/SimpleDXL.sol
+++ contracts/SimpleDXL.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -19,11 +19,17 @@ contract SimpleDEX {
         IERC20(tokenAddress).transfer(msg.sender, amount);
     }
 
-    function tradeUSDC(uint256 amount) external {
-        IERC20(usdcAddress).transferFrom(msg.sender, address(this), amount);
-        IERC20(tokenAddress).transfer(msg.sender, amount);
-    }
+    
+     function tradeUSDC(uint256 usdcAmount) external {
+        require(usdcAmount > 0, "invalid usdc amount");
 
+        // Ensure that the sender has approved this contract to spend their USDT tokens
+        IERC20 usdcToken = IERC20(usdtAddress);
+        require(
+            usdcToken.allowance(msg.sender, address(this)) >= usdcAmount,
+            "USDT allowance not set"
+        );
+     }
     function withdrawTokens(address token, uint256 amount) external {
         require(msg.sender == tokenAddress, "Only the token contract can withdraw tokens");
         IERC20(token).transfer(msg.sender, amount);
