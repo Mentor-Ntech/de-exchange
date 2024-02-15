544a17bf89bfb191925e6711cb0ca4697822ff61
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-02-15 09:14:35 +0100

    home commit

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,7 +1,20 @@
 import './Home.css'
-const Home = () => {
+import { FaDiscord, FaGithub, FaTwitter  } from "react-icons/fa";
+const Home  = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className='heroBox'>
+       <div className='heroText'>
+        <h1> <span>DLTCOIN</span> PROTOCOL</h1>
+        <p>Swap, earn, and build on the leading decentralized crypto trading protocol.</p>
+       </div>
+       <div className='iconBox'>
+         <FaDiscord />
+         <FaGithub />
+         <FaTwitter />
+       </div>
+    </div>
+    </>
   )
 }
 
